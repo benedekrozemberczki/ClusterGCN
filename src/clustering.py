ac5c17c1edc469c60b4f012ede89b354519da4ed
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2019-06-20 17:16:31 +0100

    Add files via upload

--- src/clustering.py
+++ src/clustering.py
@@ -1,8 +1,8 @@
-import random
-import networkx as nx
 import metis
-import community
 import torch
+import random
+import community
+import networkx as nx
 from sklearn.model_selection import train_test_split
 
 class ClusteringMachine(object):
@@ -13,13 +13,13 @@ class ClusteringMachine(object):
 
     def decompose(self):
         if self.args.clustering_method == "metis":
-            print("Metis graph clustering started.")
+            print("\nMetis graph clustering started.\n")
             self.metis_clustering()
         elif self.args.clustering_method == "louvain":
-            print("Louvain graph clustering started.")
+            print("\nLouvain graph clustering started.\n")
             self.louvain_clustering()
         else:
-            print("Random graph clustering started.")
+            print("\nRandom graph clustering started.\n")
             self.random_clustering()
         self.general_cluster_membership_mapping()
         self.transfer_edges_and_nodes()
@@ -31,7 +31,7 @@ class ClusteringMachine(object):
     def metis_clustering(self):
         (_, parts) = metis.part_graph(self.graph, self.args.cluster_number)
         self.clusters = list(set(parts))
-        self.cluster_membership = {node: membership for node, membership in parts}
+        self.cluster_membership = {node: membership for node, membership in enumerate(parts)}
 
     def louvain_clustering(self):
         self.cluster_membership = community.best_partition(self.graph)
@@ -48,7 +48,8 @@ class ClusteringMachine(object):
             self.sg_nodes[cluster] = [node for node in subgraph.nodes()]
             mapper = {node: i for i, node in enumerate(self.sg_nodes[cluster])}
             self.sg_edges[cluster] = [[mapper[edge[0]], mapper[edge[1]]] for edge in subgraph.edges()]
-            self.sg_train_nodes[cluster], self.sg_test_nodes[cluster] = train_test_split(list(mapper.keys()),train_size = self.args.train_ratio)
+            self.sg_train_nodes[cluster], self.sg_test_nodes[cluster] = train_test_split(list(mapper.values()),test_size = self.args.test_ratio)
+
 
     def transfer_edges_and_nodes(self):
         for cluster in self.clusters:
@@ -56,5 +57,4 @@ class ClusteringMachine(object):
             self.sg_edges[cluster] = torch.LongTensor(self.sg_edges[cluster] ).view(2,-1)
             self.sg_train_nodes[cluster] = torch.LongTensor(self.sg_train_nodes[cluster])
             self.sg_test_nodes[cluster] = torch.LongTensor(self.sg_test_nodes[cluster])
-            print(self.sg_train_nodes[cluster].shape, self.sg_edges[cluster].shape)
-    
+            
